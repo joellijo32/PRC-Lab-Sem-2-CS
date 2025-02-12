e94b9c5eb45e47da07f3616ea17b082c337557be
joellijo32 <joellijo4@gmail.com>
2025-02-12 18:43:26 +0530

    Q9 Completed.

---
+++ Cycle 2/Q9DecimaltoBinary.c
@@ -0,0 +1,28 @@
+//Experiment No. 9
+//Decimal to binary
+//Created by Joel Lijo
+#include <stdio.h>
+void main()
+{
+    printf("Enter a Decimal Integer: ");
+    int decimal;
+    scanf("%d", &decimal);
+    if(decimal < 0)
+    {
+        printf("Invalid Input. Positive Integers Only.");
+        return;
+    }
+    int binary[33]; int index = 0;
+    while(decimal >= 1){
+        int rem = decimal % 2 ;
+        decimal /= 2;
+        binary[index++] = rem;
+        
+    }
+    index--;
+    for(int i = index;i > -1;i--)
+    {
+        printf("%d", binary[i]);
+    }
+
+}
