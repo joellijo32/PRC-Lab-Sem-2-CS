96f28081246d13c19e6228e3404d041abde5b1a0
Joel Lijo Mathew <joellijo4@gmail.com>
2025-02-22 19:19:35 +0530

    Completed Q12

--- Cycle 2/Q12SetOp.c
+++ Cycle 2/Q12SetOp.c
@@ -1,63 +1,94 @@
-//Experiment No. 12
-//Set Operations
-//Created by Joel Lijo 
 #include <stdio.h>
-void main()
-{
-    printf("Number of Elements in A = ");
-    int na; 
-    scanf("%d", &na);
-    if(na < 0)
+
+int main() {
+    int n, m;
+    printf("Enter number of elements in set A: ");
+    scanf("%d", &n);
+    if(n < 0)
     {
-        printf("Invalid Input");
+        printf("Invalid Input. Negative number Entered.");
         return;
     }
-    printf("Enter the elements of A: \n");
-    int A[na];
-    for(int i =  0; i < na;i++)
-    {
+
+    int A[n];
+    printf("Enter elements of set A: ");
+    for (int i = 0; i < n; i++) {
         scanf("%d", &A[i]);
     }
-    printf("\nNumber of Elements in B = ");
-    int nb;
-    scanf("%d", &nb);
-    if(nb < 0)
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (A[i] == A[j]) {
+                printf("Error: Set A contains duplicate elements.\n");
+                return 1;
+            }
+        }
+    }
+
+
+    printf("Enter number of elements in set B: ");
+    scanf("%d", &m);
+    
+
+    if(m < 0)
     {
-        printf("Invalid Input");
+        printf("Invalid Input. Negative number Entered.");
         return;
     }
-    int B[nb];
-    printf("Enter the Elements in B: \n");
-    for(int i = 0;i < nb; i++)
-    {
+    int B[m];
+    printf("Enter elements of set B: ");
+    for (int i = 0; i < m; i++) {
         scanf("%d", &B[i]);
     }
-
-    int uni[na + nb], size = 0;
-    if(na > nb)
-    {
-        size = na;
-    }
-    else size = nb;
-
-    int intersec[size], minus[size], indb = 0, inda = 0;
-    for(int i = 0; i < (na+nb); i++)
-    {
-        if(i > (na-1))
-        {
-            uni[i] = B[indb++];
-        }
-        else
-        {
-            uni[i] = A[inda++];
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (B[i] == B[j]) {
+                printf("Error: Set B contains duplicate elements.\n");
+                return 1;
+            }
         }
     }
-    printf("Union of A and B: \n");
-    printf("{ ");
-    for(int i = 0;i < (na+nb) ; i++)
-    {
-        if(i == 0) printf("%d", uni[i]);
-        else printf(", %d", uni[i]);
+
+    printf("A union B = { ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", A[i]);
     }
-    printf(" }");
+    for (int i = 0; i < m; i++) {
+        int present = 0;
+        for (int j = 0; j < n; j++) {
+            if (B[i] == A[j]) {
+                present = 1;
+                break;
+            }
+        }
+        if (!present) {
+            printf("%d ", B[i]);
+        }
+    }
+    printf("}\n");
+    printf("A intersection B = { ");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (A[i] == B[j]) {
+                printf("%d ", A[i]);
+                break;
+            }
+        }
+    }
+    printf("}\n");
+    printf("A - B = { ");
+    for (int i = 0; i < n; i++) {
+        int present = 0;
+        for (int j = 0; j < m; j++) {
+            if (A[i] == B[j]) {
+                present = 1;
+                break;
+            }
+        }
+        if (!present) {
+            printf("%d ", A[i]);
+        }
+    }
+    printf("}\n");
+    return 0;
 }
