c9dbf1d5a804793f3c5eef56b4f32074c3221680
Joel Lijo Mathew <joellijo4@gmail.com>
2025-03-11 20:49:07 +0530

    Started Cycle 3, finishing first 2 quests

---
+++ Cycle 3/Q14LargestInMatrix.c
@@ -0,0 +1,33 @@
+//Experiment No. 14
+//Largest in a matrix
+//Created by Joel Lijo
+#include <stdio.h>
+int main(){
+    printf("No. of rows = "); int m , n; scanf("%d", &m);
+    printf("No. of columns = "); scanf("%d", &n);
+    int array[m][n];
+    printf("Enter the elements: \n");
+    for(int i = 0; i < m;i++){
+        for(int j = 0;j < n;j++){
+            scanf("%d", &array[i][j]);
+        }
+    }
+    printf("\nArray Entered: \n");
+    for(int i = 0; i < m;i++){
+        for(int j = 0; j < n;j++){
+            printf("%d ", array[i][j]);
+        }printf("\n");
+    }
+
+    int largest = 0, row = 0, cols = 0;
+    for(int i = 0; i < m;i++){
+        for(int j = 0;j < n;j++){
+            if(largest < array[i][j]){
+                largest = array[i][j];
+                row = i+1; cols = j+1;
+            }
+        }
+    }
+
+    printf("Largest array in the matrix: %d at row: %d and column: %d", largest, row, cols);
+}
---
+++ Cycle 3/Q15isSymmetric.c
@@ -0,0 +1,39 @@
+//Experiment No. 15
+//is Symmetric
+//Created by Joel Lijo
+#include <stdio.h>
+int main(){
+    printf("Enter size: "); int n;
+    scanf("%d", &n);
+    int array[n][n];
+    printf("Enter the elements: \n");
+    for(int i = 0; i < n;i++){
+        for(int j = 0;j < n;j++){
+            scanf("%d", &array[i][j]);
+        }
+    }
+    
+    int isSymmetric = 1;
+    for(int i = 0; i < n;i++){
+        for(int j = 0;j < n;j++){
+            if(array[i][j] != array[j][i]){
+                isSymmetric = 0;
+                break;
+            }
+        }
+    }
+
+    if(isSymmetric){
+        printf("The array is Symmetric");
+    }else{
+        printf("The array is NOT a Symmetric.\n");
+        printf("Transpose of the matrix: \n");
+        for(int i = 0; i < n;i++){
+            for(int j = 0;j < n;j++){
+                printf("%d ", array[j][i]);
+            } printf("\n");
+        }
+    }
+
+
+}
